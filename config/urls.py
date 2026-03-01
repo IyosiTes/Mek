71e052d58b8a181126d26c282491cf86d7fed88c
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-01 07:59:25 -0800

    url: add cart api path

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Products API
     path("api/", include("catalog.urls")),
+    #Cart API
+    path("api/cart/", include("cart.urls")),
 ]
 
 if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 

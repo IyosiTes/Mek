e4519681806aa369e2549596251dbd04d33fc872
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-08 09:46:37 -0700

    add JWT login and refresh endpoints

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +29,12 @@ urlpatterns = [
     path("api/", include("catalog.urls")),
     #Cart API
     path("api/cart/", include("cart.urls")),
+    #JWT login and refresh endpoints
+    path("api/auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
 ]
+
 
 if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

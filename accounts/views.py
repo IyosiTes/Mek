a043a4e3af4f9b5d0128cd86f5eff2c982e79d15
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-08 10:21:24 -0700

    add current user endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView, Response;
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email
+        })

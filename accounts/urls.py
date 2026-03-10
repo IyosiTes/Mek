b242fb1a668d9111e989a2fbd5a8dea745105584
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-09 23:32:55 -0700

    fixing auth Me endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import MeView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-   path("auth/me",MeView.as_view()) 
-
+   path("auth/me/", MeView.as_view()),
+   path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+   path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]

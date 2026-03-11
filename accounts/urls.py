386649e0058eb8796c405b9b7a357d99ce48081e
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-11 05:34:06 -0700

    feat(auth): created endpoint for registeration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MeView
+from .views import MeView, RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+   path("register/", RegisterView.as_view(), name="register"),
    path("auth/me/", MeView.as_view()),
    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),

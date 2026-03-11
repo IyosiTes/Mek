822ba46e704235e3c2b8c7cf386a747bd2d519c6
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-11 05:32:56 -0700

    feat(auth): added serializer for registeration

---
+++ accounts/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "phone_number",
+            "password",
+            "password_confirm",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError("Passwords do not match")
+
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            phone_number=validated_data["phone_number"],
+            password=validated_data["password"],
+        )
+
+        return user

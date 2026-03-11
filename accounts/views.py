8568f30079b23373f4e0a47d202ce476b3273660
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-11 05:33:43 -0700

    feat(auth): created RegisterView for registeration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from rest_framework.views import APIView, Response;
+from rest_framework.views import APIView, Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+
+from accounts.serializers import RegisterSerializer
 # Create your views here.
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
@@ -10,3 +13,16 @@ class MeView(APIView):
             "username": request.user.username,
             "email": request.user.email
         })
+    
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User created successfully"},
+                status=status.HTTP_201_CREATED
+            )
+
+   

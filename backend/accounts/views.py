c27f3151d6cf2ccb912cb66d1ab5ae4332f740a5
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-17 06:57:04 -0700

    upgrade view for userprofile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,17 +3,26 @@ from rest_framework.views import APIView, Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from accounts.serializers import RegisterSerializer
+from accounts.serializers import RegisterSerializer,UserSerializer
 # Create your views here.
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
+   
     def get(self, request):
-        return Response({
-            "username": request.user.username,
-            "email": request.user.email
-        })
-    
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = UserSerializer(
+            request.user, data=request.data, partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)

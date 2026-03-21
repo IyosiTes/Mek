6ef82f2e4f1103da0121dd6b265909fe24cc700c
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-21 12:50:14 -0700

    adding order list endpoint for userprofile

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateOrderView, OrderDetailView, SubmitTelebirrPaymentView
+from .views import CreateOrderView, OrderDetailView, SubmitTelebirrPaymentView, UserOrdersView
 
 urlpatterns = [
     
      path("", CreateOrderView.as_view()),
     path("<int:pk>/", OrderDetailView.as_view()),
     path("telebirr/<int:order_id>/", SubmitTelebirrPaymentView.as_view()),
+    path("list/", UserOrdersView.as_view()),
 ]

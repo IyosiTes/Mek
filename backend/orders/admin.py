79be30dbd82e4ee7274ab43324e6be61ba246c01
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-24 12:43:34 -0700

    upgrade admin order

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+from django.utils.html import format_html
 
 # Register your models here.
-from django.contrib import admin
-from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
+    readonly_fields = ("product", "quantity", "price")
+    can_delete = False
 
 
 @admin.register(Order)
@@ -21,15 +23,18 @@ class OrderAdmin(admin.ModelAdmin):
         "status",
         "total_amount",
         "created_at",
+        "payment_preview",
     )
 
-    list_filter = ("payment_method", "payment_status", "status")
+    list_filter = ("payment_method", "payment_status", "status", "created_at")
 
-    search_fields = ("order_code", "user__username", "transaction_id")
+    search_fields = ("order_code", "user__username", "phone_number", "transaction_id")
+
+    ordering = ("-created_at",)
 
     inlines = [OrderItemInline]
 
-    readonly_fields = ("order_code", "created_at")
+    readonly_fields = ("order_code", "created_at", "payment_preview",)
 
     fieldsets = (
         ("Order Info", {
@@ -47,6 +52,38 @@ class OrderAdmin(admin.ModelAdmin):
                 "payment_status",
                 "transaction_id",
                 "payment_screenshot",
+                "payment_preview",
             )
         }),
+        ("Metadata", {
+            "fields": ("created_at",)
+        }),
     )
+
+    def payment_preview(self, obj):
+        if obj.payment_screenshot:
+            return format_html(
+                '<a href="{}" target="_blank">'
+                '<img src="{}" style="height:60px;border-radius:6px;" />'
+                '</a>',
+                obj.payment_screenshot.url,
+                obj.payment_screenshot.url
+            )
+        return "No Screenshot"
+
+    payment_preview.short_description = "Payment Proof"
+
+    #  ADMIN ACTIONS 
+    actions = ["mark_as_paid", "mark_as_rejected", "mark_as_shipped"]
+
+    @admin.action(description="Mark selected orders as PAID")
+    def mark_as_paid(self, request, queryset):
+        queryset.update(payment_status="paid", status="confirmed")
+
+    @admin.action(description="Mark selected orders as REJECTED")
+    def mark_as_rejected(self, request, queryset):
+        queryset.update(payment_status="unpaid")
+
+    @admin.action(description="Mark selected orders as SHIPPED")
+    def mark_as_shipped(self, request, queryset):
+        queryset.update(status="shipped")

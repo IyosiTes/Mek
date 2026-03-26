874c9c311932949e5d7271e4caa50714b65c074c
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-26 01:11:40 -0700

    updating order url

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import CreateOrderView, OrderDetailView, SubmitTelebirrPaymentView, UserOrdersView
+from .views import CreateOrderView, OrderDetailView,  UserOrdersView
 
 urlpatterns = [
     
      path("", CreateOrderView.as_view()),
     path("<int:pk>/", OrderDetailView.as_view()),
-    path("telebirr/<int:order_id>/", SubmitTelebirrPaymentView.as_view()),
     path("list/", UserOrdersView.as_view()),
 ]

4afd9c63d6c010a3130ff4f01e8bfa949786ed50
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-03-27 23:12:16 -0700

    updating backend url to mekback for render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["mek-b5iv.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["mekback.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition

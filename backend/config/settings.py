d02d76e2b57de9a30fa32c039d117ac7d9fa1aa5
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-08 11:30:36 -0700

    configuring gmail for reset forgot password

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -199,7 +199,15 @@ SIMPLE_JWT = {
 
 
 
-# Email settings (DEV)
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Email settings (DEV..console backend, PROD..SMTP backend)
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+        
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 

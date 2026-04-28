589ff0e2b650c669453be78a78ced859a917f5b1
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-27 23:20:52 -0700

    community + admin dashboard improvements

---
+++ backend/community/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import CommentList, CommentReactionCreate, PostListCreate, CommentCreate
+
+urlpatterns = [
+    path('posts/', PostListCreate.as_view()),
+    path('posts/<int:post_id>/comment/', CommentCreate.as_view()),
+    path('posts/<int:post_id>/comments/', CommentList.as_view()),
+    path('comments/<int:comment_id>/react/', CommentReactionCreate.as_view()),
+
+]

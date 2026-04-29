2ec4a77ae54041273bb908acdd0ec7bdf6a30d50
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-29 01:18:27 -0700

    adding single post endpoint

--- backend/community/urls.py
+++ backend/community/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CommentList, CommentReactionCreate, PostListCreate, CommentCreate
+from .views import CommentList, CommentReactionCreate, PostListCreate, CommentCreate, PostRetrieveAPIView
 
 urlpatterns = [
     path('posts/', PostListCreate.as_view()),
     path('posts/<int:post_id>/comment/', CommentCreate.as_view()),
     path('posts/<int:post_id>/comments/', CommentList.as_view()),
     path('comments/<int:comment_id>/react/', CommentReactionCreate.as_view()),
-
+    path('posts/<int:post_id>/', PostRetrieveAPIView.as_view()),
 ]

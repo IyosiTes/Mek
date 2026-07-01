794d2d098810fd45186b14999afb8321ed9faa6c
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-07-01 04:24:42 -0700

    (perf) changing anon user to auth user

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -30,10 +30,9 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         user = User.objects.create_user(
             username=validated_data["username"],
-            phone_number=validated_data["phone_number"],
+            phone_number=validated_data.get("phone_number"),
             email=validated_data["email"],
-            address=validated_data["address"],
-           
+            address=validated_data.get("address"),
             password=validated_data["password"],
         )
 
@@ -49,3 +48,32 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
         ]
         read_only_fields = ["username"]
+
+class CommunityRegisterSerializer(serializers.ModelSerializer):
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password",
+            "password_confirm",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )

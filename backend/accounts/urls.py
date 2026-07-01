22d17efec477d24fdcb9624ada01e46f125a5ca7
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-07-01 04:25:02 -0700

    (perf) changing anon user to auth user

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import MeView, RegisterView, UserProfileView, UpdateProfileView, ForgotPasswordView, ResetPasswordView, HealthCheckView
+from .views import MeView, RegisterView, CommunityRegisterView, UserProfileView, UpdateProfileView, ForgotPasswordView, ResetPasswordView, HealthCheckView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
    path("register/", RegisterView.as_view(), name="register"),
+   path(
+    "community-register/",
+    CommunityRegisterView.as_view(),
+    name="community-register"
+),
    path("me/", MeView.as_view()),
    path("login/", TokenObtainPairView.as_view(), name="login"),
    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),

37b873f955bf9b9b2747f5abbcabfba1445ba8bf
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-07-05 20:03:27 -0700

    deleting unwanted end points

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
-from .views import MeView, RegisterView, CommunityRegisterView, UserProfileView, UpdateProfileView, ForgotPasswordView, ResetPasswordView, HealthCheckView
+from .views import MeView, RegisterView, ForgotPasswordView, ResetPasswordView, HealthCheckView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
    path("register/", RegisterView.as_view(), name="register"),
-   path(
-    "community-register/",
-    CommunityRegisterView.as_view(),
-    name="community-register"
-),
-   path("me/", MeView.as_view()),
    path("login/", TokenObtainPairView.as_view(), name="login"),
+   path("me/", MeView.as_view()),
    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-   path("profile/", UserProfileView.as_view()),
-   path("profile/update/", UpdateProfileView.as_view()),
    path("forgot-password/", ForgotPasswordView.as_view()),
    path("reset-password/", ResetPasswordView.as_view()),
    path("health/", HealthCheckView.as_view()),

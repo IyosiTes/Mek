cf796ca76e3267e937349e0a14612e832f7f9aef
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-07-05 20:01:55 -0700

    feat: jwt model fixation

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -7,23 +7,9 @@ from django.utils import timezone
 class User(AbstractUser):
     # Extra fields beyond Django's default
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    church_affiliation = models.CharField(max_length=200, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-
+    
     # Vendor flag (you control this in admin)
     is_vendor = models.BooleanField(default=False)
-
-    # Preferred payment method (COD or Online)
-    PAYMENT_CHOICES = [
-        ("COD", "Cash on Delivery"),
-        ("ONLINE", "Online Payment"),
-    ]
-    preferred_payment = models.CharField(
-        max_length=10,
-        choices=PAYMENT_CHOICES,
-        default="COD",
-    )
 
     def __str__(self):
         return self.username
